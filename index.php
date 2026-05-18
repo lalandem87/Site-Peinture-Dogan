3be6ce283bc8efbdcaca202d8a911943e375df5b
= <=>
2026-05-18 15:33:55 +0200

    Add hero section

--- index.php
+++ index.php
@@ -25,5 +25,61 @@ https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css
             <a class="btn-devis" href="#devis">Demander un devis</a>
         </div>
     </header>
+    <main>
+        <section id="accueil">
+            <div class="hero-container">
+                <div class="hero-left">
+                <h1>L'art de peindre
+                    vos espaces, avec
+                    <em class="passion">passion</em> et
+                    <em class="precision">précision</em>.
+                </h1>
+                <p class="hero-desc">Interventions rapides, chantiers propres et conseils
+                    couleurs personnalisés pour les particuliers et les
+                    professionnels.
+                </p>
+                <a href="#devis">Parler de mon projet</a>
+                <div class="services">
+                    <div class="service">
+                        <div class="logo house">
+                            <i class="fa-regular fa-house"></i>
+                        </div>
+                        <div class="title">Intérieur</div>
+                        <p class="desc">Murs, plafonds, boiseries.</p>
+                    </div>
+                    <div class="service">
+                        <div class="logo sun">
+                            <i class="fa-solid fa-sun"></i>
+                        </div>
+                        <div class="title">Extérieur</div>
+                        <p class="desc">Façades, volets, portails.</p>
+                    </div>
+                    <div class="service">
+                        <div class="logo roller">
+                            <i class="fa-solid fa-paint-roller"></i>
+                        </div>
+                        <div class="title">Décoration</div>
+                        <p class="desc">Papiers peints, effets.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hero-right">
+                <div class="img-wraper">
+                    <img src="./backend/images/hero-image.png" alt="image-hero">
+                    <div class="badge">
+                        <div class="logo">
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <div class="infos">
+                            <div class="title">Artisan Certifié</div>
+                            <p>Garantie décennale sur tous nos travaux.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+        </section>
+    </main>
 </body>
 </html>
