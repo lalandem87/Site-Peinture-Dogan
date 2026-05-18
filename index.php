2337a3c6a3485fc98c560bf4fb0681c1443f5035
= <=>
2026-05-18 13:50:41 +0200

    Add header

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Dogan</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link href="
+https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css
+" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="" alt="logo entreprise">
+        </div>
+        <nav>
+            <a href="#accueil">Accueil</a>
+            <a href="#realisations">Réalisations</a>
+            <a href="#prestations">Prestations</a>
+            <a href="#avis">Avis</a>
+        </nav>
+        <div class="tel-devis">
+            <span class="tel"><i class="fa-solid fa-phone"></i>01 23 45 67 89</span>
+            <a class="btn-devis" href="#devis">Demander un devis</a>
+        </div>
+    </header>
+</body>
+</html>
